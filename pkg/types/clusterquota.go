d395baaf5b2c0930ef6f8df416f5ed4fcb0bed24
anton-sidelnikov <a.sidelnikov@t-systems.com>
2023-10-03 19:49:21 +0200

    prepare-3

--- pkg/types/clusterquota.go
+++ pkg/types/clusterquota.go
@@ -1,11 +1,10 @@
 package types
 
-import (
-	"github.com/openshift/installer/pkg/types/gcp"
-)
-
 // ClusterQuota contains the size, in cloud quota, of
 // the cluster that was created by installer.
 type ClusterQuota struct {
-	GCP *gcp.Quota `json:"gcp,omitempty"`
+	Stub *StubQuota `json:"gcp,omitempty"`
+}
+
+type StubQuota struct {
 }
